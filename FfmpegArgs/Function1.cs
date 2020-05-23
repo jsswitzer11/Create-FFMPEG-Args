cc73d282f9fa38259a3b349890ceeda076a6173a
Jake Switzer <jsswitzer11@hotmail.com>
2020-05-22 22:54:48 -0400

    Updated to search through directory

--- FfmpegArgs/Function1.cs
+++ FfmpegArgs/Function1.cs
@@ -26,6 +26,7 @@ namespace FfmpegArgs
         private static string dVidName;
         private static string kVidName;
         private static string Season;
+        private static string SeasonType;
         private static string Week;
         static IQueueClient queueClient;
 
@@ -37,8 +38,8 @@ namespace FfmpegArgs
             try
             {
                 GetSettings(context, log);
-                //GetVideoBlobAsync(Season, Week);
-                GetVideoBlobAsync("rawvideos");
+                GetVideoBlobAsync();
+                //GetVideoBlobAsync("rawvideos");
                 GetPlayMetadataAsync(log);
                 return (ActionResult)new OkObjectResult("Complete");
             }
@@ -63,8 +64,7 @@ namespace FfmpegArgs
                 XmlDocument doc = new XmlDocument();
                 doc.Load(ms);
 
-                string league;
-                string SeasonType;
+                string league;                
                 string gameKey;
                 string playID;
                 string offMp4 = string.Empty;
@@ -81,9 +81,6 @@ namespace FfmpegArgs
                     if (node.Name == "Game")
                     {
                         league = node.Attributes.GetNamedItem("League").Value;
-                        Season = node.Attributes.GetNamedItem("Season").Value;
-                        SeasonType = node.Attributes.GetNamedItem("SeasonType").Value;
-                        Week = node.Attributes.GetNamedItem("Week").Value;
 
                         foreach (XmlNode game in node.ChildNodes)
                         {
@@ -157,7 +154,7 @@ namespace FfmpegArgs
 
                                             string ffmpeg_file = Path.Combine(settings.outputPath, p.outputName);
                                             string id = play.Attributes.GetNamedItem("ID").Value;
-                                            string arg = $"-ss {p.markInFrame.ToString()} -t {p.duration.ToString()} {ffmpeg_file}";
+                                            string arg = $"-ss {p.startTime.ToString()} -t {p.duration.ToString()} {ffmpeg_file}";
                                             playArgs.Add(id + " -- " + arg);
 
                                             if (play.Attributes.GetNamedItem("ID").Value == offense)
@@ -185,67 +182,40 @@ namespace FfmpegArgs
                         }
                     }
                 }
-                //string oFile = Path.Combine(settings.outputPath, "offense_ffmpeg_params.txt");
-                //string dFile = Path.Combine(settings.outputPath, "defense_ffmpeg_params.txt");
-                //string kFile = Path.Combine(settings.outputPath, "kicks_ffmpeg_params.txt");
-                //using (StreamWriter ofile = new StreamWriter(oFile))
-                //{
-                    foreach (string pa in playArgs)
+
+                foreach (string pa in playArgs)
+                {
+                    if (pa.Contains("CoachOffense"))
                     {
-                        if (pa.Contains("CoachOffense"))
-                        {
-                            queueClient = new QueueClient(settings.ServiceBusConnectionString, "offense");
-                            string offMessageBody = $"-i {oVidName} " + pa.Substring(pa.LastIndexOf("-- ") + 3);
-                            var offMessage = new Message(Encoding.UTF8.GetBytes(offMessageBody));
+                        queueClient = new QueueClient(settings.ServiceBusConnectionString, "defense");
+                        string offMessageBody = $"-i {oVidName} " + pa.Substring(pa.LastIndexOf("-- ") + 3);
+                        var offMessage = new Message(Encoding.UTF8.GetBytes(offMessageBody));
 
-                            await queueClient.SendAsync(offMessage);
+                        await queueClient.SendAsync(offMessage);
 
-                            await queueClient.CloseAsync();
-
-                            //ofile.WriteLine($"-i {oVidName} " + pa.Substring(pa.LastIndexOf("-- ") + 3));
+                        await queueClient.CloseAsync();
                     }
-                    }
-               // }
-                //using (StreamWriter dfile = new StreamWriter(dFile))
-                //{
-                    foreach (string pa in playArgs)
+                    else if (pa.Contains("CoachDefense"))
                     {
-                        if (pa.Contains("CoachDefense"))
-                        {
-                            queueClient = new QueueClient(settings.ServiceBusConnectionString, "defense");
-                            string defMessageBody = $"-i {dVidName} " + pa.Substring(pa.LastIndexOf("-- ") + 3);
-                            var defMessage = new Message(Encoding.UTF8.GetBytes(defMessageBody));
+                        queueClient = new QueueClient(settings.ServiceBusConnectionString, "offense");
+                        string defMessageBody = $"-i {dVidName} " + pa.Substring(pa.LastIndexOf("-- ") + 3);
+                        var defMessage = new Message(Encoding.UTF8.GetBytes(defMessageBody));
 
-                            await queueClient.SendAsync(defMessage);
+                        await queueClient.SendAsync(defMessage);
 
-                            await queueClient.CloseAsync();
-
-                            //dfile.WriteLine($"-i {dVidName} " + pa.Substring(pa.LastIndexOf("-- ") + 3));
+                        await queueClient.CloseAsync();
                     }
-                    }
-               // }
-                //using (StreamWriter kfile = new StreamWriter(kFile))
-                //{
-                    foreach (string pa in playArgs)
+                    else if (pa.Contains("CoachKicks"))
                     {
-                        if (pa.Contains("CoachKicks"))
-                        {
-                            queueClient = new QueueClient(settings.ServiceBusConnectionString, "specialteams");
-                            string kickMessageBody = $"-i {kVidName} " + pa.Substring(pa.LastIndexOf("-- ") + 3);
-                            var kickMessage = new Message(Encoding.UTF8.GetBytes(kickMessageBody));
+                        queueClient = new QueueClient(settings.ServiceBusConnectionString, "specialteams");
+                        string kickMessageBody = $"-i {kVidName} " + pa.Substring(pa.LastIndexOf("-- ") + 3);
+                        var kickMessage = new Message(Encoding.UTF8.GetBytes(kickMessageBody));
 
-                            await queueClient.SendAsync(kickMessage);
+                        await queueClient.SendAsync(kickMessage);
 
-                            await queueClient.CloseAsync();
-
-                            //kfile.WriteLine($"-i {kVidName} " + pa.Substring(pa.LastIndexOf("-- ") + 3));
+                        await queueClient.CloseAsync();
                     }
-                    }
-               // }
-
-                //WritePlayVideoBlob("offense_ffmpeg_params.txt", oFile, log);
-                //WritePlayVideoBlob("defense_ffmpeg_params.txt", dFile, log);
-                //WritePlayVideoBlob("kicks_ffmpeg_params.txt", kFile, log);
+                }
 
                 ms.Close();
 
@@ -257,19 +227,39 @@ namespace FfmpegArgs
 
         }
 
-        public static async void GetVideoBlobAsync(string seasonContainerName)//, string weekFileName)
+        public static async void GetVideoBlobAsync()
         {
+
+            string rawXML = await GetXMLBlob("xmlscript");
+            byte[] encodedString = Encoding.UTF8.GetBytes(rawXML);
+
+            MemoryStream memStream = new MemoryStream(encodedString);
+
+            XmlDocument xml = new XmlDocument();
+            xml.Load(memStream);
+
+            foreach (XmlNode node in xml.DocumentElement.ChildNodes)
+            {
+                if (node.Name == "Game")
+                {
+                    Season = node.Attributes.GetNamedItem("Season").Value.ToString();
+                    SeasonType = node.Attributes.GetNamedItem("SeasonType").Value;
+                    Week = node.Attributes.GetNamedItem("Week").Value.ToString();
+                }
+            }
+
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(settings.outputStorageAccountConnStr);
 
             var serviceClient = storageAccount.CreateCloudBlobClient();
 
-            CloudBlobContainer inputContainer = serviceClient.GetContainerReference($"{seasonContainerName}");
+            CloudBlobContainer inputContainer = serviceClient.GetContainerReference($"{Season}");
+            CloudBlobDirectory inputDirectory = inputContainer.GetDirectoryReference($"{SeasonType}/{Week}/raw/");
 
-            //Get file from the following format: Season/Week/raw
-            //var inputDirectory = inputContainer.GetDirectoryReference($"{weekFileName}/raw/");
+            //Get file from the following format: Season/SeasonType/Week/raw
+            //var inputDirectory = inputContainer.GetDirectoryReference($"{seasonType}/{week}/raw/");
 
-            //var blobItem = await inputDirectory.ListBlobsSegmentedAsync(null);
-            var blobItem = await inputContainer.ListBlobsSegmentedAsync(null);
+            var blobItem = await inputDirectory.ListBlobsSegmentedAsync(null);
+            //var blobItem = await inputContainer.ListBlobsSegmentedAsync(null);
             foreach (var item in blobItem.Results)
             {
                 var blob = (CloudBlockBlob)item;
@@ -309,24 +299,6 @@ namespace FfmpegArgs
             return null;
         }
 
-        //static void WritePlayVideoBlob(string filename, string filepath, ILogger log)
-        //{
-        //    try
-        //    {
-        //        BlobServiceClient blobServiceClient = new BlobServiceClient(settings.outputStorageAccountConnStr);
-        //        BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(settings.blobContainerName);
-
-        //        BlobClient blobClient = blobContainerClient.GetBlobClient(filename);
-
-        //        FileStream uploadFileStream = File.OpenRead(filepath);
-        //        blobClient.Upload(uploadFileStream);
-        //        uploadFileStream.Close();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        log.LogInformation(ex.Message);
-        //    }
-        //}
         static void GetSettings(ExecutionContext context, ILogger log)
         {
             try
@@ -352,7 +324,6 @@ namespace FfmpegArgs
                 settings.storageAccountName = config["storageAccountName"];
                 settings.sasToken = config["sasToken"];
                 settings.ServiceBusConnectionString = config["ServiceBusConnectionString"];
-                settings.blobContainerName = "ffmpegargs";
                 if (!String.IsNullOrEmpty(config["blobContainerName"]))
                 {
                     settings.blobContainerName = config["blobContainerName"];
